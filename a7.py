8ae661175ba92d60522490a3508e8af4728bc9ff
Ashley Quiroz <aquiroz24@cps.edu>
2026-04-13 10:36:19 -0500

    update

--- a7.py
+++ a7.py
@@ -108,8 +108,14 @@ class BayesClassifier:
         # avoid extra work in the future (using the save_dict method). The objects you
         # are saving are self.pos_freqs and self.neg_freqs and the filepaths to save to
         # are self.pos_filename and self.neg_filename
+        self.save_dict(self.pos_freqs, self.pos_filename)
+        self.save_dict(self.neg_freqs, self.neg_filename)
 
     def classify(self, text: str) -> str:
+        #work on this
+        """"self.classify(self.pos_freqs, )
+            self.classify(self.)"""
+           # return classify
         """Classifies given text as positive, or negative from calculating the
         most likely document class to which the target string belongs
 
